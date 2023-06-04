2f53146278c4be7125c7a327c96c51c53fa7b447
baya <abaya6054@gmail.com>
2023-06-04 10:45:44 -0700

    main api

--- server.js
+++ server.js
@@ -31,16 +31,11 @@ app.use('/api/forma', sendEmail)
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))) 
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-    app.get('*',(req,res)=>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    )
-}else{
+
 app.get('/', (req, res) => {
     res.send('api is running.....') 
 }) 
-}
+
 
 
 
